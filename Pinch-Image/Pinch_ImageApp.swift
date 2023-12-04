e7d206b3b9d57b7ffc551282d7f7294f7f9b0967
abdulwariskhan <hellokhan.ch@gmail.com>
2023-12-04 16:02:07 +0100

    Initial Commit

---
+++ Pinch-Image/Pinch_ImageApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pinch_ImageApp.swift
+//  Pinch-Image
+//
+//  Created by Khan on 04.12.23.
+//
+
+import SwiftUI
+
+@main
+struct Pinch_ImageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
