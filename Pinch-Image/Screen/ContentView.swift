906ed10830b69ec36c607cee8658bd7ff88eacc9
abdulwariskhan <hellokhan.ch@gmail.com>
2023-12-08 19:31:57 +0100

    implemented DragGesture method

--- Pinch-Image/Screen/ContentView.swift
+++ Pinch-Image/Screen/ContentView.swift
@@ -45,6 +45,15 @@ struct ContentView: View {
                                 imageOffset = value.translation
                             }
                         }
+                        .onEnded { _ in
+                            if imageScale <= 1 {
+                                
+                                withAnimation(.spring()) {
+                                    imageScale = 1
+                                    imageOffset = .zero
+                                }
+                            }
+                        }
                     )
                 
             } // : ZSTACK
