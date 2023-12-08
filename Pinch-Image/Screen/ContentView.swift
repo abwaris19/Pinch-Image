59a90582e110d4d740b8f09da0ec83b793cd5c19
abdulwariskhan <hellokhan.ch@gmail.com>
2023-12-08 19:18:33 +0100

    implemented Double Tap Gasture

--- Pinch-Image/Screen/ContentView.swift
+++ Pinch-Image/Screen/ContentView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct ContentView: View {
     
     @State private var isAnimating: Bool = false
-    
+    @State private var imageScale : CGFloat = 1
     var body: some View {
      
         NavigationView {
@@ -23,13 +23,27 @@ struct ContentView: View {
                     .cornerRadius(10)
                     .shadow(color: Color.black.opacity(0.2), radius: 12, x: 2, y: 2 )
                     .opacity(isAnimating ? 1 :  0)
+                    .scaleEffect(imageScale)
+                    .onTapGesture(count: 2, perform: {
+                        if imageScale == 1 {
+                            withAnimation(.spring()) {
+                                imageScale = 5
+                            }
+                        }
+                        else {
+                            withAnimation(.spring()) {
+                                imageScale = 1
+                            }
+                        }
+                    })
+                    .padding()
                 
             } // : ZSTACK
             .navigationTitle("Pinch & Zoom")
             .navigationBarTitleDisplayMode(.inline)
             .onAppear(perform: {
                 withAnimation(.linear(duration: 1)) {
-                    isAnimating = true 
+                    isAnimating = true
                 }
             })
         } //: Navigation
