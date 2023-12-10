b7eaa5d2cd6f0942a2195d618c9a91713091b6e6
abdulwariskhan <hellokhan.ch@gmail.com>
2023-12-10 21:22:34 +0100

    created InfoPanelView file

---
+++ Pinch-Image/View/InfoPanelView.swift
@@ -0,0 +1,23 @@
+//
+//  InfoPanelView.swift
+//  Pinch-Image
+//
+//  Created by Khan on 10.12.23.
+//
+
+import SwiftUI
+
+struct InfoPanelView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct InfoPanelView_Previews: PreviewProvider {
+    static var previews: some View {
+        InfoPanelView()
+            .preferredColorScheme(.dark)
+            .previewLayout(.sizeThatFits)
+            .padding()
+    }
+}
