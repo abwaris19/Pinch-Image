7a58ffa22d41ccc40c5463bfadabc0fe50475b3e
abdulwariskhan <hellokhan.ch@gmail.com>
2023-12-10 21:33:25 +0100

    arrange Image with values

--- Pinch-Image/View/InfoPanelView.swift
+++ Pinch-Image/View/InfoPanelView.swift
@@ -8,6 +8,9 @@
 import SwiftUI
 
 struct InfoPanelView: View {
+    
+    var scale: CGFloat
+    var offset: CGSize
     var body: some View {
         
         HStack {
@@ -18,13 +21,37 @@ struct InfoPanelView: View {
                 .frame(width: 30, height: 30)
             
             Spacer()
+            
+            HStack(spacing: 2 ) {
+                Image(systemName: "arrow.up.left.and.arrow.down.right")
+                Text("\(scale)")
+                
+                Spacer()
+                
+                Image(systemName: "arrow.left.and.right")
+                Text("\(offset.width)")
+                
+                Spacer()
+                
+                Image(systemName: "arrow.up.and.down")
+                Text("\(offset.height)")
+                
+                Spacer()
+            
+            }
+            .font(.footnote)
+            .padding(8)
+            .background(.ultraThinMaterial)
+            .cornerRadius(8)
+            .frame(maxWidth: 420)
+            Spacer()
         }
     }
 }
 
 struct InfoPanelView_Previews: PreviewProvider {
     static var previews: some View {
-        InfoPanelView()
+        InfoPanelView(scale: 1, offset: .zero)
             .preferredColorScheme(.dark)
             .previewLayout(.sizeThatFits)
             .padding()
