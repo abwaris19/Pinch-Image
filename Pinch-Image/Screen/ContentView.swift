1970ed2ee9752b6bdb338b3e338d737b8eb9f08a
abdulwariskhan <hellokhan.ch@gmail.com>
2023-12-11 20:25:13 +0100

    Drawer User Interface

--- Pinch-Image/Screen/ContentView.swift
+++ Pinch-Image/Screen/ContentView.swift
@@ -12,6 +12,7 @@ struct ContentView: View {
     @State private var isAnimating: Bool = false
     @State private var imageScale : CGFloat = 1
     @State private var imageOffset: CGSize = .zero
+    @State private var isDrawerOpen: Bool = false
     
     func resetImageState(){
         return withAnimation(.spring()) {
@@ -166,7 +167,38 @@ struct ContentView: View {
                     .padding(.bottom, 30),
                 alignment: .bottom
             )
-           
+           // MARK: DRAWER
+            
+            .overlay(
+            
+                HStack (spacing: 12 ){
+                    
+                    // MARK: - DRAWER HANDLE
+                    
+                    Image(systemName: isDrawerOpen ?  "chevron.compact.right" : "chevron.compact.left")
+                        .padding(8)
+                        .foregroundStyle(.secondary)
+                        .onTapGesture (perform: {
+                          
+                            withAnimation(.easeOut) {
+                                isDrawerOpen.toggle()
+                            }
+                        })
+                    
+                  // MARK: - THUMBNAILS
+                    
+                    Spacer()
+                }
+                    .padding(EdgeInsets(top: 16, leading: 8, bottom: 16, trailing: 8))
+                    .background(.ultraThinMaterial)
+                    .cornerRadius(12)
+                    .opacity(isAnimating ? 1 : 0)
+                    .frame(width: 260)
+                    .padding(.top, UIScreen.main.bounds.height / 12)
+                    .offset(x: isDrawerOpen ? 20: 215)
+                    , alignment: .topTrailing
+                    
+            )
             
         } //: Navigation
         .navigationViewStyle(.stack)
