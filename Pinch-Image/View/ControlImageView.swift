b0bb33456d7214f11ab1bb8ac0428854fb9ef351
abdulwariskhan <hellokhan.ch@gmail.com>
2023-12-11 16:23:21 +0100

    created ControlImageView file

---
+++ Pinch-Image/View/ControlImageView.swift
@@ -0,0 +1,20 @@
+//
+//  ControlImageView.swift
+//  Pinch-Image
+//
+//  Created by Khan on 11.12.23.
+//
+
+import SwiftUI
+
+struct ControlImageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ControlImageView_Previews: PreviewProvider {
+    static var previews: some View {
+        ControlImageView()
+    }
+}
